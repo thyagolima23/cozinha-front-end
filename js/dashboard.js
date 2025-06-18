184aebdd32b54ba574352855955b2d3f6a468637
thyagolima23 <thyago.lima2324@gmail.com>
2025-06-17 21:55:02 -0300

    update

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,4 @@
-const imgbbAPIKey = 'SUA_API_KEY_DO_IMGBB'; // Substitua pela sua chave real
+const imgbbAPIKey = '9a38d36d162032e65231552d4af958fe'; // Substitua pela sua chave real
 
 const fileInput = document.getElementById('img');
 const hiddenInputUrl = document.getElementById('img-url');
